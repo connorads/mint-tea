83c29a7f2b9e083c7149505f3cec9defbf203a3d
Connor Adams <connorads@users.noreply.github.com>
2022-01-17 22:25:03 +0000

    Base64 encode JSON and image

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -28,7 +28,10 @@ contract MyEpicNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/F5XR");
+        _setTokenURI(
+            newItemId,
+            "data:application/json;base64,eyJuYW1lIjoiQnVtYmFjbGFydCBCYWRtYW4iLCJkZXNjcmlwdGlvbiI6IkFjdHVhbGx5IGZvcmdldCBhIHN0cmVldCBuYW1lIGNhbGwgbWUgSm9zZXBoIEknbSBhIEJ1bWJhY2xhcnQgQmFkbWFuIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStRblZ0WW1GamJHRnlkRUpoWkcxaGJqd3ZkR1Y0ZEQ0S1BDOXpkbWMrIiwiYnJvc2tpIjoiSk1FIn0="
+        );
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
